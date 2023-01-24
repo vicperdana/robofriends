2aa1e220a427616b2d84bc6a1c17416c7c197878
Vic Perdana (MSFT) <7114832+vicperdana@users.noreply.github.com>
2023-01-25 03:02:59 +1100

    Updates

---
+++ precache-manifest.c15b2e3e38b528d5b1bbb3b722a9b777.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ca23f9afbd6f61085dd",
+    "url": "/robofriends/static/css/main.4492ac95.chunk.css"
+  },
+  {
+    "revision": "2ca23f9afbd6f61085dd",
+    "url": "/robofriends/static/js/main.2ca23f9a.chunk.js"
+  },
+  {
+    "revision": "9ae850e2c227a6cf397e",
+    "url": "/robofriends/static/css/1.a609dcaf.chunk.css"
+  },
+  {
+    "revision": "9ae850e2c227a6cf397e",
+    "url": "/robofriends/static/js/1.9ae850e2.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ec3e5ecab255c6e2d756af859dcc05bd",
+    "url": "/robofriends/index.html"
+  }
+];
